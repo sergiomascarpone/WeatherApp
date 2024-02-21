3384f6bd1792bf57c839502e99d3256d0ed27801
Serjo Bykov <serjobikov@gmail.com>
2024-02-21 15:51:24 +0300

    Add

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Sergio Mascarpone on 21.02.24.
+//
+
+import SnapKit
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
