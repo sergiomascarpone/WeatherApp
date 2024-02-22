8761eed6bba2607ecc5cfabb0e18f76849382253
Serjo Bykov <serjobikov@gmail.com>
2024-02-22 19:22:38 +0300

    AddViewsAndIcons

--- WeatherApp/WeatherApp/ViewController/TodayViewController.swift
+++ WeatherApp/WeatherApp/ViewController/TodayViewController.swift
@@ -15,8 +15,16 @@ class TodayViewController: UIViewController {
     private lazy var cityNameLocationLabel = UILabel()
     private lazy var descriptionLabel = UILabel()
     private lazy var humidView = UIImageView()
+    private lazy var humidLabel = UILabel()
+//    private lazy var stackViewInfoHumid = UIStackView()
     private lazy var windSpeedView = UIImageView()
+    private lazy var windSpeedLabel = UILabel()
     private lazy var thermoView = UIImageView()
+    private lazy var thermoLabel = UILabel()
+    private lazy var pressureView = UIImageView()
+    private lazy var pressureLabel = UILabel()
+    private lazy var windSockView = UIImageView()
+    private lazy var windSockLabel = UILabel()
     
 
     override func viewDidLoad() {
@@ -26,6 +34,7 @@ class TodayViewController: UIViewController {
         
     }
     
+//MARK: - SetUpViews
     private func initialize() {
         view.backgroundColor = .cyan
         
@@ -44,13 +53,147 @@ class TodayViewController: UIViewController {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().inset(60)
         }
-
+        
         //imageView
-        humidView.image = UIImage(systemName: "humidity.fill")
-        view.addSubview(humidView)
-        humidView.snp.makeConstraints { maker in
+        imageView.image = UIImage(named: "Sun")
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { maker in
+            maker.height.equalTo(100)
+            maker.width.equalTo(100)
             maker.centerX.equalToSuperview()
             maker.top.equalToSuperview().inset(100)
+        }
+        
+        //cityNameLocationLabel
+        cityNameLocationLabel.text = "Cupertino | US"
+        cityNameLocationLabel.font = UIFont(name: "AmericanTypewriter", size: 30)
+        view.addSubview(cityNameLocationLabel)
+        cityNameLocationLabel.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.top.equalToSuperview().inset(220)
+        }
+        
+        //descriptionLabel
+        descriptionLabel.text = "13.38 °C | Clear"
+        descriptionLabel.font = UIFont(name: "AmericanTypewriter", size: 26)
+        view.addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.top.equalToSuperview().inset(280)
+        }
+        
+        //stackViewInfoHumid
+//        let view = UIStackView(arrangedSubviews: [humidView,
+//                                                  humidLabel
+//        ])
+//        
+//        view.axis = .vertical
+//        view.spacing = 6
+//        view.alignment = .center
+//        view.distribution = .fill
+//        
+//        view.addSubview(stackViewInfoHumid)
+//        
+//        stackViewInfoHumid.snp.makeConstraints { maker in
+//            maker.height.equalTo(80)
+//            maker.width.equalTo(80)
+//            maker.left.equalToSuperview().inset(40)
+//            maker.top.equalToSuperview().inset(350)
+//        }
+
+        //humidView
+        humidView.image = UIImage(named: "Humid")
+        view.addSubview(humidView)
+        humidView.snp.makeConstraints { maker in
+            maker.height.equalTo(80)
+            maker.width.equalTo(80)
+            maker.left.equalToSuperview().inset(40)
+            maker.top.equalToSuperview().inset(350)
+        }
+        
+        //humidLabel
+        humidLabel.text = "0 mm"
+        humidLabel.font = UIFont(name: "Times New Roman", size: 22)
+        view.addSubview(humidLabel)
+        humidLabel.snp.makeConstraints { maker in
+            maker.left.equalToSuperview().inset(55)
+            maker.top.equalToSuperview().inset(440)
+        }
+        
+        //windSpeedView
+        windSpeedView.image = UIImage(named: "Windy-Speed")
+        view.addSubview(windSpeedView)
+        windSpeedView.snp.makeConstraints { maker in
+            maker.height.equalTo(80)
+            maker.width.equalTo(80)
+            maker.centerX.equalToSuperview()
+            maker.top.equalToSuperview().inset(350)
+        }
+        
+        //windSpeedLabel
+        windSpeedLabel.text = "0.45 km/h"
+        windSpeedLabel.font = UIFont(name: "Times New Roman", size: 22)
+        view.addSubview(windSpeedLabel)
+        windSpeedLabel.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.top.equalToSuperview().inset(440)
+        }
+        
+        //thermoView
+        thermoView.image = UIImage(named: "Temperature")
+        view.addSubview(thermoView)
+        thermoView.snp.makeConstraints { maker in
+            maker.height.equalTo(80)
+            maker.width.equalTo(80)
+            maker.right.equalToSuperview().inset(40)
+            maker.top.equalToSuperview().inset(350)
+        }
+        
+        //thermoLabel
+        thermoLabel.text = "69 %"
+        thermoLabel.font = UIFont(name: "Times New Roman", size: 22)
+        view.addSubview(thermoLabel)
+        thermoLabel.snp.makeConstraints { maker in
+            maker.right.equalToSuperview().inset(55)
+            maker.top.equalToSuperview().inset(440)
+        }
+        
+        //pressureView
+        pressureView.image = UIImage(named: "Pressure")
+        view.addSubview(pressureView)
+        pressureView.snp.makeConstraints { maker in
+            maker.height.equalTo(70)
+            maker.width.equalTo(70)
+            maker.left.equalToSuperview().inset(80)
+            maker.top.equalToSuperview().inset(480)
+        }
+        
+        //pressureLabel
+        pressureLabel.text = "1021 hPa"
+        pressureLabel.font = UIFont(name: "Times New Roman", size: 22)
+        view.addSubview(pressureLabel)
+        pressureLabel.snp.makeConstraints { maker in
+            maker.left.equalToSuperview().inset(75)
+            maker.top.equalToSuperview().inset(560)
+        }
+        
+        //windSockView
+        windSockView.image = UIImage(named: "WindSock")
+        view.addSubview(windSockView)
+        windSockView.snp.makeConstraints { maker in
+            maker.height.equalTo(70)
+            maker.width.equalTo(70)
+            maker.right.equalToSuperview().inset(80)
+            maker.top.equalToSuperview().inset(480)
+        }
+        
+        //windSockLabel
+        windSockLabel.text = "S"
+        windSockLabel.font = UIFont(name: "Times New Roman", size: 22)
+        view.addSubview(windSockLabel)
+        windSockLabel.snp.makeConstraints { maker in
+            maker.right.equalToSuperview().inset(75)
+            maker.top.equalToSuperview().inset(560)
         }
         
     }
