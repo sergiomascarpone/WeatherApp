960e60f52ef5c0f73d6ed3e2852cd929268c9fbe
Serjo Bykov <serjobikov@gmail.com>
2024-03-05 20:51:21 +0300

    WeatherCase

--- WeatherApp/WeatherApp/WeatherCase.swift
+++ WeatherApp/WeatherApp/WeatherCase.swift
@@ -3,29 +3,29 @@
 //  WeatherApp
 //
 //  Created by Sergio Mascarpone on 29.02.24.
-//
-//import Foundation
-//import UIKit
-//
-//enum WeatherCase {
-//    case humid
-//    case wind
-//    case temperature
-//    case pressure
-//    case windsock
-//    
-//    var image: UIImage? {
-//        switch self {
-//        case .humid:
-//            return UIImage(systemName:"humid")
-//        case .wind:
-//            return UIImage(systemName:"wind")
-//        case .temperature:
-//            return UIImage(systemName:"temperature")
-//        case .pressure:
-//            return UIImage(systemName:"pressure")
-//        case .windsock:
-//            return UIImage(systemName:"windSock")
-//        }
-//    }
-//}
+
+import Foundation
+import UIKit
+
+enum WeatherCase {
+    case humid
+    case wind
+    case temperature
+    case pressure
+    case windsock
+    
+    var image: UIImage? {
+        switch self {
+        case .humid:
+            return UIImage(systemName:"humid")
+        case .wind:
+            return UIImage(systemName:"wind")
+        case .temperature:
+            return UIImage(systemName:"temperature")
+        case .pressure:
+            return UIImage(systemName:"pressure")
+        case .windsock:
+            return UIImage(systemName:"windSock")
+        }
+    }
+}
