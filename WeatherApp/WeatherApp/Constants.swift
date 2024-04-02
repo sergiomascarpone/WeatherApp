5db7f445900af88c2c35f938436afb6a0005da83
Serjo Bykov <serjobikov@gmail.com>
2024-04-02 14:13:29 +0300

    addCommit: правки

---
+++ WeatherApp/WeatherApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Sergio Mascarpone on 2.04.24.
+//
+
+import Foundation
+
+enum Constants: String, RawRepresentable {
+    case defaultURL = "https://api.openweathermap.org/data/2.5/"
+    case apiKey = "70b9166dc33204c62ee3c1e299ac88ee"
+    case latitudesKey = "latitudes"
+    case longitudesKey = "longitudes"
+}
---
+++ WeatherApp/WeatherApp/WeatherService.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Sergio Mascarpone on 2.04.24.
+//
+
+import Foundation
