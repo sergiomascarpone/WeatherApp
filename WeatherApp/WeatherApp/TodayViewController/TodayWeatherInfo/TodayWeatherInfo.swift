e757939cbf08eaf64c50d29e4358470e633e77d1
Serjo Bykov <serjobikov@gmail.com>
2024-04-03 15:52:42 +0300

    addCommit:add .gitignore

---
+++ WeatherApp/WeatherApp/TodayViewController/TodayWeatherInfo/TodayWeatherInfo.swift
@@ -0,0 +1,29 @@
+//
+//  AnotherViewController.swift
+//  WeatherApp
+//
+//  Created by Sergio Mascarpone on 3.04.24.
+//
+
+import UIKit
+
+class TodayWeatherInfo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
