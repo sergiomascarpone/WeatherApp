fa2e10fb116e335ca52f1c6f0969df2bfdcc5ea9
Serjo Bykov <serjobikov@gmail.com>
2024-04-08 11:58:59 +0300

    add:TodayWeatherInfo.xib

--- WeatherApp/WeatherApp/TodayViewController/TodayWeatherInfo/TodayWeatherInfo.swift
+++ WeatherApp/WeatherApp/TodayViewController/TodayWeatherInfo/TodayWeatherInfo.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class TodayWeatherInfo: UIViewController {
 
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var humidView: UIStackView!
+    @IBOutlet weak var windyView: UIStackView!
+    @IBOutlet weak var termometerView: UIStackView!
+    @IBOutlet weak var pressureView: UIStackView!
+    @IBOutlet weak var windSockView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
