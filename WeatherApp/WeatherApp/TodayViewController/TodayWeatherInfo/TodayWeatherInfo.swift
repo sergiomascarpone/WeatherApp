95fb35df21dc7b4f1699b310e7049702abfcb0b4
Serjo Bykov <serjobikov@gmail.com>
2024-04-10 13:38:40 +0300

    addCommit: TodayWeatherInfo.xib (разбор и подключение xib file)

--- WeatherApp/WeatherApp/TodayViewController/TodayWeatherInfo/TodayWeatherInfo.swift
+++ WeatherApp/WeatherApp/TodayViewController/TodayWeatherInfo/TodayWeatherInfo.swift
@@ -7,17 +7,34 @@
 
 import UIKit
 
-class TodayWeatherInfo: UIViewController {
-
-    @IBOutlet var contentView: UIView!
-    @IBOutlet weak var humidView: UIStackView!
-    @IBOutlet weak var windyView: UIStackView!
-    @IBOutlet weak var termometerView: UIStackView!
-    @IBOutlet weak var pressureView: UIStackView!
-    @IBOutlet weak var windSockView: UIStackView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class TodayWeatherInfo: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+       super .init(coder: aDecoder)
+    }
+    
+    private func setupViews() {
+        let xibView = loadViewFromXib()
+        xibView.frame = self.bounds
+        xibView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(xibView)
+    }
+    
+    private func loadViewFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "TodayWeatherInfo", bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first! as! UIView
+    }
+
+//    @IBOutlet var contentView: UIView!
+//    @IBOutlet weak var humidView: UIStackView!
+//    @IBOutlet weak var windyView: UIStackView!
+//    @IBOutlet weak var termometerView: UIStackView!
+//    @IBOutlet weak var pressureView: UIStackView!
+//    @IBOutlet weak var windSockView: UIStackView!
 
 }
