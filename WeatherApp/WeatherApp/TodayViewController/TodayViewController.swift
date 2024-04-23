1831e0f702004f895c86fa3c8446c44a1288fec4
Serjo Bykov <serjobikov@gmail.com>
2024-04-23 15:10:10 +0300

    updateTodayViewController\ПреобразованиеНаправленияВетра

--- WeatherApp/WeatherApp/TodayViewController/TodayViewController.swift
+++ WeatherApp/WeatherApp/TodayViewController/TodayViewController.swift
@@ -78,9 +78,12 @@ class TodayViewController: UIViewController {
         self.todayWeatherInfoView.windyLabel.text = "\(weatherData.wind.speed) km/h"
         self.todayWeatherInfoView.termometerLabel.text = "\(weatherData.main.humidity) mm"
         self.todayWeatherInfoView.pressureLabel.text = "\(weatherData.main.pressure) hPa"
-        self.todayWeatherInfoView.windSockLabel.text = "\(weatherData.wind.deg)"
+        
+        //Вывод и конвертирование, направления ветра.
+        let windDirection = presenter.defineWindDirection(degree: weatherData.wind.deg)
+        self.todayWeatherInfoView.windSockLabel.text = windDirection
 
-        //Вывод иконки по описанию погоды
+        //Вывод иконки по описанию погоды.
         self.imageView.image = UIImage(named: weatherImageName(for: weatherData.weather.first?.description ?? ""))
     }
     
